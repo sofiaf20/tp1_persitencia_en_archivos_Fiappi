a8d507323533429df692b93bc3cdc33e9222338b
sofiaf20 <69475001+sofiaf20@users.noreply.github.com>
2021-05-18 14:02:24 -0300

    Create puntajes_pickle.01.3.py

---
+++ ejemplos_en_python/puntajes_pickle.01.3.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# encoding: utf8
+
+import pickle
+
+def guardar_puntajes(nombre_archivo, puntajes):
+    """ Guarda la lista de puntajes en el archivo.
+    Pre: nombre_archivo corresponde a un archivo válido,
+         puntajes corresponde a una lista de tuplas de 3 elementos.
+    Post: se guardaron los valores en el archivo,
+          separados por comas.
+    """
+    archivo = open(nombre_archivo, "w")
+    pickle.dump(puntajes, archivo)
+    archivo.close()
+
+def recuperar_puntajes(nombre_archivo):
+    """ Recupera los puntajes a partir del archivo provisto.
+        Devuelve una lista con los valores de los puntajes.
+    Pre: el archivo contiene los puntajes en el formato esperado,
+         separados por comas
+    Post: la lista devuelta contiene los puntajes en el formato:
+          [(nombre1,puntaje1,tiempo1),(nombre2,puntaje2,tiempo2)].
+    """
+    archivo = open(nombre_archivo, "r")
+    puntajes = pickle.load(archivo)
+    archivo.close()
+    return puntajes
+
+valores = [("Pepe", 108, "4:16"), ("Juana", 2315, "8:42")]
+guardar_puntajes("puntajes.txt", valores)
+print (recuperar_puntajes("puntajes.txt"))
